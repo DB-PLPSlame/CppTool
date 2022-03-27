ff09d258e0842260a86f2fb0197d868582e7ec3f
summer0o0o <77971776+summer0o0o@users.noreply.github.com>
2022-03-27 13:36:54 +0800

    Delete Python directory

--- Python/Grammar/1_Demo/demo05.py
+++
@@ -1,6 +0,0 @@
-# print()函数，默认在末尾添加'\n'换行
-# 参数end = "任意字符串"，设置末尾添加
-
-print("aa", end = '*')
-print("bb")
-print("cc")
--- Python/Grammar/numpy/04_numpy.py
+++
@@ -1,8 +0,0 @@
-# 数组转置
-import numpy as np
-
-arr = np.arange(15).reshape((3, 5))
-print(arr.dot(arr.T))
-
-# 4.2 通用函数：快速的元素级数组函数
-
